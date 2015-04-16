bcb676a4c75541f93e07ee8b67a34b7d094f237b
Drew Ross <andrew.jross89@gmail.com>
2015-04-16 15:21:49 -0500

    hooked in gramophone image

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php include("includes/header.php"); ?>
 
     <div class="hero hero--light">
-        <img src="https://placeimg.com/1680/864/nature" alt="Placeholder" />
+        <img src="build/images/Berliner-Disc-1897.jpg" alt="Placeholder" />
         <div class="wrapper">
             <div class="hero__content">
                 <div class="text-group">
-                    <h1 class="hdg hdg--1">Hello World</h1>
+                    <!-- <h1 class="hdg hdg--1">boilerplate</h1>
                     <div>
                         <a class="link" href="http://www.gramophone.roztocki.com/" target="_blank">Get Started</a>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
