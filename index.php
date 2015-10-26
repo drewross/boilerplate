71bf3b7d290f547c895c2196f79d8175cc42e912
Drew Ross <andrew.jross89@gmail.com>
2015-10-26 15:11:27 -0500

    removing unused images

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include("includes/header.php"); ?>
 
     <div class="hero hero--fixed js-sectionFull">
-        <img src="build/images/gramophone-logo-mark.jpg" alt="Gramophone, a front end boilerplate" />
+        <img src="http://gramophone.roztocki.com/gramophone-logo-mark.jpg" alt="Gramophone, a front end boilerplate" />
         <div class="hero__content">
             <div class="wrapper text-center">
                 <div class="text-group  fadeIn">
